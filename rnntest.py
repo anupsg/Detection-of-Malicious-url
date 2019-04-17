3ee965e36c15d13e7d9b6511cac25361179b393c
niveshmavuduru123 <49075881+niveshmavuduru123@users.noreply.github.com>
2019-04-17 17:55:50 -0500

    2nd commit.

--- rnntest.py
+++ rnntest.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from sklearn.cross_validation import train_test_split
 import pandas as pd
 import numpy as np
+np.random.seed(1337)
 from keras.preprocessing import sequence
 from keras.utils import np_utils
 from keras.models import Sequential
@@ -10,8 +11,19 @@ from keras.layers import LSTM, SimpleRNN, GRU
 from keras.datasets import imdb
 from keras.utils.np_utils import to_categorical
 from sklearn.metrics import (precision_score, recall_score,f1_score, accuracy_score,mean_squared_error,mean_absolute_error)
+from sklearn import metrics
+from sklearn.preprocessing import Normalizer
+import h5py
 from keras import callbacks
 from keras.callbacks import CSVLogger
+import keras
+import keras.preprocessing.text
+import itertools
+from keras.callbacks import CSVLogger
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, CSVLogger
+from keras import callbacks
+from keras.layers import Convolution1D, GlobalMaxPooling1D
+from keras.preprocessing.text import Tokenizer
 
 trainlabels = pd.read_csv('data/testlabel.csv', header=None)
 trainlabel = trainlabels.iloc[:,0:1]
@@ -22,6 +34,18 @@ testlabel = testlabels.iloc[:,0:1]
 
 train = pd.read_csv('data/test.txt', header=None)
 test = pd.read_csv('data/test.txt', header=None)
+
+
+
+X = train.values.tolist()
+X = list(itertools.chain(*X))
+
+
+T = test.values.tolist()
+T = list(itertools.chain(*T))
+
+
+
 
 # Generate a dictionary of valid characters
 valid_chars = {x:idx+1 for idx, x in enumerate(set(''.join(X+T)))}
@@ -45,3 +69,33 @@ X_test = sequence.pad_sequences(T1, maxlen=maxlen)
 
 y_train = np.array(trainlabel)
 y_test = np.array(testlabel)
+
+
+embedding_vecor_length = 128
+
+model = Sequential()
+model.add(Embedding(max_features, embedding_vecor_length, input_length=maxlen))
+model.add(SimpleRNN(128))
+model.add(Dropout(0.1))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.load_weights("logs/rnn/checkpoint-18.hdf5")
+
+y_pred = model.predict_classes(X_test)
+y_proba = model.predict_proba(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred , average="binary")
+precision = precision_score(y_test, y_pred , average="binary")
+f1 = f1_score(y_test, y_pred, average="binary")
+
+print("confusion matrix")
+print("----------------------------------------------")
+print("accuracy")
+print("%.3f" %accuracy)
+print("racall")
+print("%.3f" %recall)
+print("precision")
+print("%.3f" %precision)
+print("f1score")
+print("%.3f" %f1)
