fd93cd0b1a78e92b23cdf4529d33304eb220aecc
Anup S G <anupsg03@gmail.com>
2019-04-22 20:41:35 -0500

    Add files via upload

--- cnn.py
+++ cnn.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
 import pandas as pd
 import numpy as np
 np.random.seed(1337)
@@ -28,3 +28,42 @@ from keras.layers import Convolution1D, GlobalMaxPooling1D
 
 trainlabels = pd.read_csv('data/trainlabel.csv', header=None)
 trainlabel = trainlabels.iloc[:,0:1]
+
+testlabels = pd.read_csv('data/testlabel.csv', header=None)
+testlabel = testlabels.iloc[:,0:1]
+
+
+
+train = pd.read_csv('data/train.txt', header=None)
+test = pd.read_csv('data/test.txt', header=None)
+
+
+
+X = train.values.tolist()
+X = list(itertools.chain(*X))
+
+
+T = test.values.tolist()
+T = list(itertools.chain(*T))
+
+
+
+
+# Generate a dictionary of valid characters
+valid_chars = {x:idx+1 for idx, x in enumerate(set(''.join(X+T)))}
+
+max_features = len(valid_chars) + 1
+
+maxlen = np.max([len(x) for x in X])
+print(maxlen)
+
+
+# Convert characters to int and pad
+X1 = [[valid_chars[y] for y in x] for x in X]
+
+T1 = [[valid_chars[y] for y in x] for x in T]
+
+
+X_train = sequence.pad_sequences(X1, maxlen=maxlen)
+
+X_test = sequence.pad_sequences(T1, maxlen=maxlen)
